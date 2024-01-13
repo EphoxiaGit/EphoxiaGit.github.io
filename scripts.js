4c2a3b9ce2880ba17f4be545e925df94e65a3711
EphoxiaGit <70481511+EphoxiaGit@users.noreply.github.com>
2024-01-13 18:06:05 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+  $('#stl_file').on('change', function() {
     $.ajax({
-        url: "/",
-       
+      url: "/",
+      method: "POST",
+      data: new FormData(this),
+      enctype: 'multipart/form-data',
+      processData: false,
+      contentType: false,
+      success: function(response) {
+        $('#result').html(response);
+      }
+    });
+  });
+});
