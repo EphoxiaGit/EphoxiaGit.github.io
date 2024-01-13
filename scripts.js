faf9b3b2f1a0bb9e80602c5773c41311eb688891
EphoxiaGit <70481511+EphoxiaGit@users.noreply.github.com>
2024-01-13 18:16:58 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,28 @@
 $(document).ready(function() {
-  $('#stl_file').on('change', function() {
-    $.ajax({
-      url: "/",
-      method: "POST",
-      data: new FormData(this),
-      enctype: 'multipart/form-data',
-      processData: false,
-      contentType: false,
-      success: function(response) {
-        $('#result').html(response);
-      }
+    const uploadForm = $("#upload-form");
+    const submitButton = $("#submit-button");
+    const resultDiv = $("#result");
+
+    uploadForm.on("submit", function(event) {
+        event.preventDefault();
+
+        const stlFile = $("#stl-file")[0].files[0];
+
+        if (!stlFile) {
+            alert("Please select an STL file");
+            return;
+        }
+
+        $.ajax({
+            url: "/",
+            method: "POST",
+            data: new FormData(this),
+            enctype: "multipart/form-data",
+            processData: false,
+            contentType: false,
+            success: function(response) {
+                resultDiv.html(response);
+            }
+        });
     });
-  });
 });
